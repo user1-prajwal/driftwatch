8f5d32178328b813a7c02aba725287c979cca84f
user1-prajwal <ewitewit451@gmail.com>
2026-05-28 04:24:36 +0530

    route 11 delete monitor

--- backend/main.py
+++ backend/main.py
@@ -261,4 +261,10 @@ def resume(monitor_id: str):
     add_monitor_to_scheduler(monitor)
     return {"message": f"Monitor {monitor_id} resumed."}
  
- 
+# ROUTE 11 — Delete a monitor
+@app.delete("/monitors/{monitor_id}")
+def delete(monitor_id: str):
+    remove_monitor_from_scheduler(monitor_id)
+    if not delete_monitor(monitor_id):
+        raise HTTPException(status_code=404, detail="Monitor not found.")
+    return {"message": f"Monitor {monitor_id} deleted."}
